677ae95a1805acbf9e58e754162315e95ba6825e
Cristian Eduardo <cristianeduardo.dev@gmail.com>
2022-03-10 23:36:12 -0300

    Use cases & Repositories

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -0,0 +1,21 @@
+import { Response, Request } from 'express';
+
+import { CreateCategoryUseCase } from './CreateCategoryUseCase';
+
+class CreateCategoryController {
+  constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
+
+  handle(request: Request, response: Response) {
+    const { name, description } = request.body;
+
+    try {
+      this.createCategoryUseCase.execute({ name, description });
+    } catch (e) {
+      return response.status(400).json({ error: e.message });
+    }
+
+    return response.status(201).send();
+  }
+}
+
+export { CreateCategoryController };
---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import { SpecificationsRepository } from '../modules/cars/repositories/SpecificationsRepository';
+import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
+
+const specificationsRoutes = Router();
+
+const specificationsRepository = new SpecificationsRepository();
+
+specificationsRoutes.post('/', (request, response) => {
+  const { name, description } = request.body;
+
+  const createSpecificationService = new CreateSpecificationService(
+    specificationsRepository,
+  );
+
+  try {
+    createSpecificationService.execute({
+      name,
+      description,
+    });
+  } catch (e) {
+    return response.status(400).json({ error: e.message });
+  }
+
+  return response.status(201).send();
+});
+
+export { specificationsRoutes };
